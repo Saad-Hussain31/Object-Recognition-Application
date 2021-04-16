a9319837ab1c55b53f3d1b968c9d412ba2c25628
Saad Hussain <saadnasir31@gmail.com>
2021-04-16 15:53:30 +0500

    added random seed

--- parameter initialization.py
+++ parameter initialization.py
@@ -1,14 +1,8 @@
 
 def initialize_parameters():
-   
-    
-    tf.set_random_seed(1)                              # so that your "random" numbers match ours
-        
-   
+    tf.set_random_seed(1)                              
     W1 = tf.get_variable("W1", [4, 4, 3,  8], initializer=tf.contrib.layers.xavier_initializer(seed = 0))
     W2 = tf.get_variable("W2", [2, 2, 8, 16], initializer=tf.contrib.layers.xavier_initializer(seed = 0))
-    
-
     parameters = {"W1": W1,
                   "W2": W2}
     
